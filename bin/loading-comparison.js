f1420e12e913f42c8c27d12815c0b406e6ee8fa1
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-02-04 19:56:03 +0100

    Make the code testable.
    Use factory functions to bind all dependencies of the modules as parameters.

--- bin/loading-comparison.js
+++ bin/loading-comparison.js
@@ -1,8 +1,23 @@
 #!/usr/bin/env node
 const args = require(`args`);
+const chalk = require(`chalk`);
+const exec = require(`child_process`).exec;
+const fs = require(`fs`);
+const path = require(`path`);
+const Table = require(`cli-table`);
 
-const browsertime = require(`../lib/browsertime`);
-const logResult = require(`../lib/log-result`);
+const executorFactory = require(`../lib/executor`);
+const browsertimeFactory = require(`../lib/browsertime`);
+const formatValuesFactory = require(`../lib/format-values`);
+const logResultFactory = require(`../lib/log-result`);
+
+const executor = executorFactory({ exec });
+const browsertime = browsertimeFactory({ fs, path, executor });
+const formatValues = formatValuesFactory({ chalk });
+const logResult = logResultFactory({ chalk, Table, formatValues });
+
+const resultDirectory = path.resolve(__dirname, `../results`);
+const resultFile = `browsertime.json`;
 
 args
   .option(
@@ -30,6 +45,8 @@ const urls = options.url.split(` `).filter(url => url.length);
 
 let previousPromise = Promise.resolve();
 Promise.all(urls.map((url) => {
-  previousPromise = previousPromise.then(() => browsertime(url, options));
+  previousPromise = previousPromise.then(
+    () => browsertime(url, options, resultDirectory, resultFile)
+  );
   return previousPromise;
 })).then(results => logResult(results, urls));
