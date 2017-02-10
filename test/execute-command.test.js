13077c9cc3b037ae4fbea5b22132ddc4d31ae2c0
Markus Oberlehner <markus.oberlehner@karriere.at>
2017-02-10 11:05:08 +0100

    Rename the executor to executeCommand.

--- test/execute-command.test.js
+++ test/execute-command.test.js
@@ -2,23 +2,23 @@ import 'babel-polyfill';
 import sinon from 'sinon';
 import test from 'ava';
 
-import { executor } from '../lib/executor';
+import { executeCommand } from '../lib/execute-command';
 
 test(`Should be a function.`, (t) => {
-  t.is(typeof executor, `function`);
+  t.is(typeof executeCommand, `function`);
 });
 
 test(`Should return a promise.`, (t) => {
   const exec = () => {};
 
-  t.is(typeof executor({ exec }, `fake command`).then, `function`);
+  t.is(typeof executeCommand({ exec }, `fake command`).then, `function`);
 });
 
 test(`Should call the exec function with the given command.`, (t) => {
   const exec = sinon.spy();
   const command = `fake command`;
 
-  executor({ exec }, command);
+  executeCommand({ exec }, command);
   t.true(exec.calledWith(command));
 });
 
@@ -26,6 +26,6 @@ test(`Should call the resolver function.`, async (t) => {
   const exec = (command, callback = () => {}) => callback();
   const resolver = sinon.spy();
 
-  executor({ exec }, `fake command`, resolver);
+  executeCommand({ exec }, `fake command`, resolver);
   t.true(await resolver.called);
 });
